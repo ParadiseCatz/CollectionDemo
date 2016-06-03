916e5fa08a1f8f2bc9d05d9a8d7dfd6cf73a6585
Anthony <anthony@29imac.local>
2016-06-03 10:04:28 +0700

    Initial Commit

---
+++ CollectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionDemo
+//
+//  Created by Anthony on 6/3/16.
+//  Copyright © 2016 Anthony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionDemo
+//
+//  Created by Anthony on 6/3/16.
+//  Copyright © 2016 Anthony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
